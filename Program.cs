7d774ee7412500dab458d82c3d4d6e5f436fe55f
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-17 21:20:25 +0300

    Решение задачи№3

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите значеие: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Введено чётное значение");
+}
+else
+Console.WriteLine("Введено нечётное значение");
